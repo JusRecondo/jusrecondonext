9bffc458749f3361956024b5553ea8d81a070d4e
JusRecondo <justina.recondo@gmail.com>
2026-03-22 14:18:29 -0300

    feat: add loader to iframe

--- src/components/gallery-item/GalleryItem.tsx
+++ src/components/gallery-item/GalleryItem.tsx
@@ -1,11 +1,12 @@
-"use client";
+'use client';
 import Image from 'next/image';
 import type { GalleryItem } from '../../../types/gallery';
 import styles from './GalleryItem.module.scss';
 import { useState } from 'react';
+import IframeWithLoader from '../iframe-with-loader/IframeWithLoader';
 
 interface Props {
-  item: GalleryItem; 
+  item: GalleryItem;
 }
 
 const GalleryItem = ({ item }: Props) => {
@@ -18,6 +19,42 @@ const GalleryItem = ({ item }: Props) => {
       onMouseLeave={() => setShowInfo(false)}
     >
       {item.type === 'embed' ? (
+        <IframeWithLoader
+          className={styles.iframe}
+          src={item.content}
+          title={item.info}
+          allowFullScreen
+        />
+      ) : item.type === 'image' ? (
+        <>
+          <p className={`${styles.itemInfo} ${showInfo && styles.showInfo}`}>
+            {item.info}
+          </p>
+          <Image
+            width={450}
+            height={450}
+            className={styles.image}
+            src={item.content}
+            alt={item.info}
+          />
+        </>
+      ) : item.type === 'video' ? (
+        <IframeWithLoader
+          className={styles.video}
+          src={item.content}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
+      ) : null}
+    </article>
+  );
+};
+
+export default GalleryItem;
+
+/* 
         <iframe
           className={styles.iframe}
           src={item.content}
@@ -25,15 +62,8 @@ const GalleryItem = ({ item }: Props) => {
           allowFullScreen
           title={item.info}
         ></iframe>
-      ) : item.type === 'image' ? (
-        <>
-          <p className={`${styles.itemInfo} ${showInfo && styles.showInfo}`}>
-            {item.info}
-          </p>
-          <Image width={450} height={450} className={styles.image} src={item.content} alt={item.info} />
-        </>
-      ) : item.type === 'video' ? (
-        <iframe
+
+<iframe
           className={styles.video}
           src={item.content}
           title="YouTube video player"
@@ -41,10 +71,4 @@ const GalleryItem = ({ item }: Props) => {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
-        ></iframe>
-      ) : null}
-    </article>
-  );
-};
-
-export default GalleryItem;
+        ></iframe> */
---
+++ src/components/iframe-with-loader/IframeWithLoader.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { HTMLAttributeReferrerPolicy, useState } from 'react';
+import { Oval } from 'react-loader-spinner';
+
+type Props = {
+  src: string;
+  title: string;
+  className?: string;
+  allow?: string;
+  referrerPolicy?: HTMLAttributeReferrerPolicy | undefined;
+  allowFullScreen?: boolean;
+};
+
+export default function IframeWithLoader({
+  src,
+  title,
+  className,
+  allow,
+  referrerPolicy,
+  allowFullScreen,
+}: Props) {
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <div style={{ position: 'relative' }} className={className}>
+      {loading && (
+          <Oval
+            visible={loading}
+            height="80"
+            width="80"
+            color="#ffffff"
+            secondaryColor="#000000"
+            ariaLabel="oval-loading"
+            wrapperStyle={{position: 'absolute',
+              inset: 0,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              background: 'rgba(0,0,0,0.2)',
+              backdropFilter: 'blur(4px)',
+              zIndex: 1,}}
+            />
+      )}
+
+      <iframe
+        src={src}
+        title={title}
+        onLoad={() => setLoading(false)}
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+        }}
+        allow={allow}
+        referrerPolicy={referrerPolicy}
+        allowFullScreen
+      />
+    </div>
+  );
+}
